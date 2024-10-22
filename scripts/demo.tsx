b01c019ee7b387f84b564f6055c965ee253c95b0
sbmsr <sebastian.messier@gmail.com>
2024-10-22 14:45:55 -0400

    dynamic grid
    grid now dynamically expands into multiple rows once >4 functions are
    loaded.

--- scripts/demo.tsx
+++ scripts/demo.tsx
@@ -96,7 +96,12 @@ app.get("/", (c) => {
                   subscribe();
                 }
               </script>`}
-        <div class="grid">
+        <div class="grid" style={{
+          "grid-template-rows": "auto",
+          "grid-template-columns": "repeat(4, 1fr)",
+          "gap": "1rem 1rem",
+          "grid-auto-rows": "100px"
+        }}>
           {sv.ids.map((id) => <Iframe id={id} />)}
         </div>
       </>
